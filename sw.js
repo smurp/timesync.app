4d56f50f9df3489d07d28650fe0a3b98b1737da1
Shawn Murphy <smurp@smurp.com>
2025-09-06 12:57:32 -0700

    Release v0.6.2

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // TimeSync PWA Service Worker with Smart Update Strategy
-const CACHE_VERSION = '0.6.1';
+const CACHE_VERSION = '0.6.2';
 const CACHE_NAME = `timesync-v${CACHE_VERSION}`;
 
 const urlsToCache = [
